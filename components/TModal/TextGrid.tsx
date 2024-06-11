107d7fac228c3de94e0ee0181033783ae040340b
Oleg Domanov <odomanov@yandex.ru>
2024-06-11 13:35:49 +0700

    Modified   components/TModal/TextGrid.tsx Modified   styles/modal.css

--- components/TModal/TextGrid.tsx
+++ components/TModal/TextGrid.tsx
@@ -3,17 +3,17 @@ import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
 
 const layout = [
-  { i: "a", x: 0, y: 0, w: 3, h: 1, static: true },
-  { i: "b", x: 3, y: 0, w: 2, h: 1, static: false },
-  { i: "c", x: 5, y: 0, w: 2, h: 1 },
-  { i: "d", x: 7, y: 0, w: 2, h: 1 },
-  { i: "e", x: 9, y: 0, w: 2, h: 1 }
+  { i: "a", x: 0, y: 0, w: 5, h: 1, static: true },
+  { i: "b", x: 5, y: 0, w: 3, h: 1, static: false },
+  { i: "c", x: 8, y: 0, w: 3, h: 1 },
+  { i: "d", x: 11, y: 0, w: 3, h: 1 },
+  { i: "e", x: 14, y: 0, w: 3, h: 1 }
 ];
 
 export const MyGrid = () => {
   return (
-      <GridLayout layout={layout} cols={12} rowHeight={300} width={1000}>
-        <div key="a" className="card-static" >aaaaaa aaaa</div>
+      <GridLayout layout={layout} cols={18} rowHeight={300} width={1000}>
+        <div key="a" className="card" >aaaaaa aaaa</div>
         <div key="b" className="card" >bbbb</div>
         <div key="c">c</div>
         <div key="d">ddddd</div>
