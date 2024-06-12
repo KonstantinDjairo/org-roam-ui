d482a2d3ef734098aee756f95d294e1d39e15c0c
Oleg Domanov <odomanov@yandex.ru>
2024-06-12 22:15:19 +0700

    Modified   components/TModal/TextGrid.tsx Added      components/TModal/AddRemove.tsx

--- components/TModal/TextGrid.tsx
+++ components/TModal/TextGrid.tsx
@@ -1,9 +1,10 @@
-import GridLayout from "react-grid-layout";
+//import GridLayout from "react-grid-layout";
+import AddRemoveLayout from './AddRemove.tsx'
 import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
 
 export const TextGrid = (props) => {
-  const layout = props.layout
+  const layout = []  //props.layout
   const divs = layout.map((l) =>
     <div key={l.i} className="card">
       {l.i}
@@ -11,8 +12,22 @@ export const TextGrid = (props) => {
   );
 
   return (
-    <GridLayout layout={layout} cols={18} rowHeight={300} width={1000}>
-      {divs}
-    </GridLayout>
+    <AddRemoveLayout
+      layout={layout}
+      //cols={18}
+      rowHeight={300}
+      width={1000}
+      containerPadding={[20,20]}
+      //allowOverlap={true}
+      //preventCollision={true}
+      compactType={null}
+      isDroppable={true}
+    onLayoutChange={function() {}}
+    //props={null}
+      //for Firefox
+      //onDragStart={e => e.dataTransfer.setData('text/plain', '')}
+    >
+      //{divs}
+    </AddRemoveLayout>
   );
 };
